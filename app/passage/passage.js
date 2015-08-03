2e0aae9b7a453bae59020831648111e48f25357c
Hailey Foster <haileyhasmail@gmail.com>
2015-08-03 15:49:11 -0700

    Add capitalize function to passage.js --> how to invoke from template?

--- app/passage/passage.js
+++ app/passage/passage.js
@@ -2,5 +2,11 @@ angular.module('hackLibsApp.passage', [])
 
 .controller('PassageController', function ($scope) {
   // Your code here
-  $scope.word = 'hello';
+  $scope.words = {noun:['stool', 'ocean', 'butt'],
+    verb:['wiggle'],
+    number:['ten', 'three']
+     };
+  $scope.capitalize = function(word){
+    return word.charAt(0).toUpperCase + word.slice(1);
+  };
 });
